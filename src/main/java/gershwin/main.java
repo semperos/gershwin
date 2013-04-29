10dad0c64718b8778d4f0b10d568d2c595eff1e5
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-04-29 14:48:33 -0400

    Fixup into "Correct how Gershwin is loaded..."

--- src/main/java/gershwin/main.java
+++ src/main/java/gershwin/main.java
@@ -1,10 +1,18 @@
 package gershwin;
 
+import clojure.lang.Symbol;
+import clojure.lang.Var;
+
 import java.io.IOException;
 
 public class main {
+    final static private Var REQUIRE = clojure.lang.RT.var("clojure.core", "require");
+    final static private Symbol CLOJURE_MAIN = Symbol.intern("clojure.main");
+    final static private Symbol GERSHWIN_MAIN = Symbol.intern("gershwin.main");
+    final static private Var MAIN = clojure.lang.RT.var("gershwin.main", "main");
 
     public static void main(String[] args) throws ClassNotFoundException, IOException {
-        clojure.lang.RT.load("gershwin-repl");
+        REQUIRE.invoke(GERSHWIN_MAIN);
+        MAIN.applyTo(clojure.lang.RT.seq(args));
     }
 }

75773e49779d5a37106cfc0482ae0733fb96541f
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-04-29 14:46:44 -0400

    Correct how Gershwin is loaded, start with REPL
    This now ensures that var's that Clojure needs having a binding before Gershwin gets rolling. Involved copying over clojure/main.clj which does that work, then tweaking the POM to pass in appropriate arguments, pointing it at the "main" namespace of gershwin/replloader.clj.

--- src/main/clj/gershwin/replloader.clj
+++ src/main/clj/gershwin/replloader.clj
@@ -1,8 +1,7 @@
-(ns gershwin-repl)
+(ns gershwin.replloader
+  (:gen-class))
 
-(defn main
+(defn -main
   "Start a Gershwin REPL. This is required to piggy-back on the Clojure environment properly."
   [& args]
   (gershwin.GershwinRepl/main args))
-
-(main)

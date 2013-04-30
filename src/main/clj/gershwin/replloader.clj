63dffbd761fcc2ad2b43566c17808ccd88854b38
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-04-30 12:22:34 -0400

    Load Gershwin in REPL 'user' namespace by default

--- src/main/clj/gershwin/replloader.clj
+++ src/main/clj/gershwin/replloader.clj
@@ -1,8 +1,9 @@
 (ns gershwin.replloader
+  (:import gershwin.GershwinRepl)
   (:gen-class))
 
 (defn -main
-  "Start a Gershwin REPL. This is required to piggy-back on the Clojure environment properly."
+  "Start a Gershwin REPL. This function is executed from within the 'user' namespace in gershwin/main.clj, hence the runtime call to the gershwin-require Clojure function to pull in Gershwin itself."
   [& args]
-  (gershwin.core/gershwin-compile 'gershwin.core)
-  (gershwin.GershwinRepl/main args))
+  (gershwin.core/gershwin-require '[gershwin.core :refer :all])
+  (GershwinRepl/main args))

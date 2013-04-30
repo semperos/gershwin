12c579b93c0462416d74ebd3ff825b248cd4ec51
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-04-30 12:17:25 -0400

    Ensure gershwin.core has been compiled to Clojure before starting REPL

--- src/main/clj/gershwin/replloader.clj
+++ src/main/clj/gershwin/replloader.clj
@@ -4,4 +4,5 @@
 (defn -main
   "Start a Gershwin REPL. This is required to piggy-back on the Clojure environment properly."
   [& args]
+  (gershwin.core/gershwin-compile 'gershwin.core)
   (gershwin.GershwinRepl/main args))
